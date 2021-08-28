a8562c5e30b0a59cfe5e7487c4bdd9ef70d11c35
Nagesh <bnageswararao6@gmail.com>
2021-08-28 20:39:47 +0530

    added new controller

---
+++ BYBootCampAssignment/src/main/java/com/example/bootcamp/Controller/siricontroller.java
@@ -0,0 +1,18 @@
+package com.example.bootcamp.Controller;
+
+public class siricontroller {
+	
+	public static void main(String[] args) {
+		
+		int a =	10;	
+		int b= 20;
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
